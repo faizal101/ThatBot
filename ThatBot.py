3849ff7292066759cb517cef91173c235dc9509c
faizal101 <Faizal@Macintosh.local>
2018-03-06 20:47:16 +0000

    few new fight strings, server count + format fixes

--- ThatBot.py
+++ ThatBot.py
@@ -18,7 +18,7 @@ grilinfo = ['__Ena Komiya from Just Because!__',
             '__Rin Shibuya from THE iDOLM@STER__',
             '__Charlotte Dunois from Infinite Stratos__',
             '__Megumin from Kono Subarashii Sekai ni Shukufuku wo!__']
-strings = [', but instead slipped on some jam and fell right into the placed hole.',
+strings = [', but instead slipped on some jam and fell right into a conveniently placed hole.',
          ' with a transformer.', ', but creates a black hole and gets sucked in.',
          ' with poutine.', ', but they slipped on a banana peel.',
          ' and in the end, the only victor was the coffin maker.',
@@ -28,21 +28,22 @@ strings = [', but instead slipped on some jam and fell right into the placed hol
          ', but they hurt themselves in confusion.', '. HADOUKEN!', ' with a pillow.',
          ' with a large fish.', ', but they stumbled over their shoelaces.', ', but they missed.',
          ' with a burnt piece of toast.', ', but it wasn\'t every effective.',
-           'and it was super effective!']
+           'and it was super effective!', ', but nothing happened.', ', while shouting out a move as if they\'re a main character from an anime.', ', but gave up halfway.']
 
 logging.basicConfig(level=logging.INFO)
 
+
 @bot.event
 async def on_ready():
-    print('Logged in as {0}'.format(bot.user))
+    print('Logged in as {}'.format(bot.user))
     print(bot.user.id)
     print('------')
     await bot.change_presence(game=discord.Game(type=2, name='kradness & reol'))  # type 0 = default, 1= streaming, 2=listening to 3= watching
 
 
 @bot.command()
-async def test(ctx, arg):
-    await ctx.send(arg)
+async def say(ctx, *, message: str):
+    await ctx.send(message)
 
 @bot.command()
 async def ping(ctx):
@@ -81,11 +82,14 @@ async def yay(ctx):
 @bot.command()
 async def fight(ctx, user: discord.Member):
     string = random.choice(strings)
-    await ctx.send(' {} is fighting {}{}'.format(ctx.author.mention, user.mention, string))
+    await ctx.send('{} is fighting {}{}'.format(ctx.author.mention, user.mention, string))
 
 @bot.command()
 async def info(ctx):
-    emb = (discord.Embed(title='Online', description='A bot made for fun by Kuuhaku#4503', colour=0xEC40DF))
+    servercount = len(bot.guilds)
+    emb = (discord.Embed(title='{}'.format(bot.user), description=
+    'A bot made for fun by Kuuhaku#4503 \nCoded in discord.py rewrite'
+    '\nThis bot is in: {} servers'.format(servercount), colour=0xEC40DF))
     await ctx.send(embed=emb)
 
 @bot.command()
@@ -93,10 +97,10 @@ async def commands(ctx):
     emb = (discord.Embed(title="List of commands: ", description=
         '.coin -Flips a coin \n.smug -Posts a smug '
         '\n.yay –umu vs uwu vs owo. winner: +o+  \n.commands \-Shows this list '
-        '\n.info –Shows information about the bot \n.invite –invite link to add this bot to your server if you want I guess '
+        '\n.info –Shows information about the bot \n. –invite link to add this bot to your server if you want I guess '
         '\n.bestgril –Best gril for you may/may not be the same as mine \n.headpat –The world needs more headpats'
         '\n.fight –Fight with someone in this server', colour=0xEC40DF))
     await ctx.send(embed=emb)
 
 
-bot.run('token')  # token goes here
+bot.run('')  # token goes here
