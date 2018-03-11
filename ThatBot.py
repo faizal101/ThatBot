9e589250ad0cd10b5e9a6118de0cee67803e54fe
faizal101 <faizal_10@live.co.uk>
2018-03-11 21:29:38 +0000

    I'm actually using the build in help command now
    I guess it looks much neater and it gets the job done.
    But i'm definitely gonna have to make my own help command one day.

--- ThatBot.py
+++ ThatBot.py
@@ -30,21 +30,25 @@ async def on_ready():
 
 @bot.command()
 async def say(ctx, *, message: str):
+    """I say what you'll say"""
     await ctx.send(message)
 
 @bot.command()
 async def ping(ctx):
+    """A simple ping command"""
     resp = await ctx.send('Pong! Loading...')
     diff = resp.created_at - ctx.message.created_at
     await resp.edit(content='Pong! That took {}ms!'.format(1000*diff.total_seconds()))
 
 @bot.command()
 async def coin(ctx):
+    """A simple coin flip"""
     flip = random.choice(['heads', 'tails'])
     await ctx.send(flip)
 
 @bot.command()
 async def smug(ctx):
+    """Posts a random smug pic"""
     with open('pathname.txt') as f:
         lines = f.readlines()
         path = (lines[4]).split('\n')[0]
@@ -54,6 +58,7 @@ async def smug(ctx):
 
 @bot.command()
 async def bestgril(ctx):
+    """Posts a random picture of the best girl from an anime"""
     with open('pathname.txt') as f:
         lines = f.readlines()
         path = (lines[4]).split('\n')[0]
@@ -64,6 +69,7 @@ async def bestgril(ctx):
 
 @bot.command()
 async def headpat(ctx):
+    """Posts a random picture of a qt anime gril getting a headpat"""
     with open('pathname.txt') as f:
         lines = f.readlines()
         headpat_path = (lines[7]).split('\n')[0]
@@ -73,6 +79,7 @@ async def headpat(ctx):
 
 @bot.command()
 async def yay(ctx):
+    """owo vs omo vs umu vs uwu, winner is +o+"""
     with open('pathname.txt') as f:
         lines = f.readlines()
         path = (lines[4]).split('\n')[0]
@@ -82,29 +89,22 @@ async def yay(ctx):
 
 @bot.command()
 async def fight(ctx, user: discord.Member):
+    """Fight a user on the server"""
     from stringChoice import fight
     await ctx.send('{} is fighting {}{}'.format(ctx.author.mention, user.mention, fight()))
 
 @bot.command()
 async def info(ctx):
+    """Information about this bot"""
     servercount = len(bot.guilds)
     emb = (discord.Embed(title='{}'.format(bot.user), description=
     'A bot made for fun by Kuuhaku#4503 \nCoded in discord.py rewrite'
     '\nThis bot is in: {} servers'.format(servercount), colour=0xEC40DF))
     await ctx.send(embed=emb)
 
-@bot.command()
-async def commands(ctx):
-    emb = (discord.Embed(title="List of commands: ", description=
-        '.coin -Flips a coin \n.smug -Posts a smug '
-        '\n.yay –umu vs uwu vs owo. winner: +o+  \n.commands \-Shows this list '
-        '\n.info –Shows information about the bot \n. –Invite link to add this bot to your server if you want I guess '
-        '\n.bestgril –Best gril for you may/may not be the same as mine \n.headpat –The world needs more headpats'
-        '\n.fight –Fight with someone in this server', colour=0xEC40DF))
-    await ctx.send(embed=emb)
-
 @bot.command(name='8ball')
 async def _8ball(ctx, *, message: str):
+    """The magic ball knows the answer to everything"""
     from stringChoice import ball
     user = str(ctx.author).split('#')[0]
     await ctx.send('{} asked: {} \n8ball: {}'.format(user, message, ball()))
